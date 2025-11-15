6c11944cab4ed7281fa916b4004ead7f9635a353
AbdullahDashti1 <abdullamustafadashti.0@gmail.com>
2025-11-15 12:35:35 +0300

    landing page now includes description for the guest user to see

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,8 +3,34 @@
 const Landing = () => {
   return (
     <main>
-      <h1>Hello, you are on the landing page for visitors.</h1>
-      <p>Sign up now, or sign in to see your super secret dashboard!</p>
+      <section>
+        <h1>Welcome to Cashly Guest!</h1>
+        <h2>Take Control of Your Finances Quickly and Securely</h2>
+        <h3>
+          With Cashly, you can create accounts, track transactions, and manage your budget all in one application
+        </h3>
+      </section>
+
+      <section>
+        <h2>Key Features</h2>
+        <ul>
+          <li>
+            <strong>Easy Account Management:</strong> Create multiple accounts for check, savings, and salaries and keep your financial data organized.
+          </li>
+          <li>
+            <strong>Track Transactions Effortlessly:</strong> Add income, expenses, and transfers, categorize them, and get smarter insights into your spending.
+          </li>
+          <li>
+            <strong>Budgeting Made Simple:</strong> Set budgets for categories like groceries, entertainment, or savings and compare actual spending with planned budgets using visual charts.
+          </li>
+          <li>
+            <strong>Insights at a Glance:</strong> View total income vs expenses and quickly understand where your money is going.
+          </li>
+          <li>
+            <strong>Safe & Private:</strong> Your financial data stays secure and accessible only when you sign in.
+          </li>
+        </ul>
+      </section>
     </main>
   );
 };
